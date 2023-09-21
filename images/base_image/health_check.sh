851e76e2c73d3a13a9685637e0a4555a757bcd23
lvn-Malystryx <malystryx@levana.exchange>
2023-09-21 22:15:35 +0800

    Add extra newline for some content does not have it

--- images/base_image/health_check.sh
+++ images/base_image/health_check.sh
@@ -6,29 +6,37 @@ now="$(date +%s)"
 now_min="$(( now / 60 ))"
 
 # Die when target is unreachable. Cannot use -O to save 500 http response.
-curl --write-out "%{http_code}" -Ls "${target}" > "/tmp/${now_min}" || exit 1
+curl --write-out "\n%{http_code}" -Ls "${target}" > "/tmp/${now_min}" || exit 1
 http_code="$(tail -1 "/tmp/${now_min}")"
 # This should not happen
-[ "${http_code}" -eq 404 ] && exit 1
+if [ "${http_code}" -eq 404 ]; then
+    exit 1
+fi
 
+# further parsing /status
 if [ "${target##*/}" = 'status' ]; then
-    # further parsing /status
+    # when grep gets no result, the error file is not created. Hence touch to keep logic going on.
     grep 'Status: ERROR$' "/tmp/${now_min}" > "/tmp/${now_min}.error" || touch "/tmp/${now_min}.error"
     five="$(find /tmp/ -maxdepth 1 -mmin +5 -name '*.error' -exec stat -c '%Z %n' {} \; | sort -n | tail -1 | cut -d' ' -f2 || true)"
     if [ -n "${five}" ]; then
         same="$(diff "/tmp/${now_min}.error" "${five}" | grep -c '^ ' || true)" # same > 0 means there were same errors 5 mins ago
-        [ "${same}" -gt 0 ] && exit 1
+        if [ "${same}" -gt 0 ]; then
+            exit 1
+        fi
     fi
 fi
 
-# Main process must tee its output to /tmp/log
-# bash -c 'mkdir -p /tmp/log && cd /tmp/log; main 2>&1 | tee >(split -b 10000 -)'
+# Main process must tee its output to /tmp/log via logging.sh
 if [ -e /tmp/log/ ]; then
     cd /tmp/log/
     last_file="$(ls -t | head -1)"
     last_change="$(stat -c%Z "${last_file}")"
     for i in *; do
-        [ "${i}" != "${last_file}" ] && rm -f "${i}"
+        if [ "${i}" != "${last_file}" ]; then
+            rm -f "${i}"
+        fi
     done
-    [ "$(( now - last_change ))" -gt 120 ] && exit 1
+    if [ "$(( now - last_change ))" -gt 120 ]; then
+        exit 1
+    fi
 fi
